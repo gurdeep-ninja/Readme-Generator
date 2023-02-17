d023ee4cd16390c6c89fb587cca64a3d3ab9daf9
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-02-17 17:34:48 +0000

    Started adding comment and commited generated README.md file as an example

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+// node.js require function to include modules
+// Include the file system module
 const fs = require("fs");
+
+// Include the path module
 const path = require('path');
+
+// Include the inquirer module
 const inquirer = require("inquirer");
+
+// Include a custom module that generates our markdown code
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
@@ -19,12 +27,12 @@ const questions = [
         type: 'input',
         message: 'Add installation guidelines:',
         name: 'installation',
-    },  
+    },
     {
         type: 'input',
         message: 'Add usage / instructions:',
         name: 'usage',
-    },  
+    },
     {
         type: 'list',
         message: 'Select a license type:',
@@ -35,49 +43,65 @@ const questions = [
             "Mozilla Public License 2.0"
         ],
         name: 'license',
-    },  
+    },
     {
         type: 'input',
         message: 'Please enter how others can contribute:',
         name: 'contributing',
-    },  
+    },
     {
         type: 'input',
         message: 'Add test instructions: ',
         name: 'tests',
-    },  
+    },
     {
         type: 'input',
         message: 'Your github username: ',
         name: 'github',
-    },  
+    },
     {
         type: 'input',
         message: 'Your email address: ',
         name: 'email',
-    },  
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
 
-    fs.writeFile(`output${path.sep}${fileName}`,data,(error) =>
-    error ? console.error (error) : console.log(`${fileName} created successfully!`)
+    // Ise the writeFile() method of the fs module to write the file with data
+    // the Path module is used to determine the file system separator (i.e Windows/Linux etc)
+    fs.writeFile(`output${path.sep}${fileName}`, data, (error) =>
+    
+        // Upon the call back (success or failure), display the result to the user
+        // An error is displayed if theres an issue
+        // If successful, the file name is displayed to the user
+        error ? console.error(error) : console.log(`${fileName} created successfully!`)
     )
 }
 
 // function to initialize program
 function init() {
+    // using the inquirer module to prompt user questions regarding the README.md file to be generated.
+
     inquirer
-    .prompt(questions)
-    .then((response) => {
+        // The prompt is taking an array of objects which has all the questions
+        .prompt(questions)
 
-      const data = generateMarkdown(response);
+        // When the user finishes answering the questions, proceed with the response data returned 
+        .then((response) => {
 
-      writeToFile("README.md",data);
+            // call the generateMarkDown() function by passing the user input and assign to a variable called data
+            // data will be returned as a string containing MARKDOWN textwhich is used to write the README.md file
 
-    }
-    );    
+            const data = generateMarkdown(response);
+
+            // Call the writeToFile() function
+            // First parameters is the file name we would like to write to
+            // Second parameter is the contents we would like to wring into the file.
+            writeToFile("README.md", data);
+        }
+        );
 
 }
 
---
+++ output/README.md
@@ -0,0 +1,37 @@
+
+
+# This is my project ![license](https://img.shields.io/badge/license-MIT-blue)
+
+## Description
+The project  will help you save time to write a README.md file
+
+### Table of Contents
+[Installation](#installation)<br>
+[Usage](#usage)<br>
+[License](#license)<br>
+[Contributing](#contributing)<br>
+[Tests](#tests)<br>
+[Questions](#questions)<br>
+
+## Installation
+Clone the repository to either your remote or local server runnig node.js
+
+## Usage
+run the command node index.js to start the file generation process
+
+## License
+This app uses the following license: MIT License
+
+## Contributing
+You can contribute by forking this repository and making pull requests.
+
+## Tests
+Test 1,2,3
+
+## Questions
+
+If you have any questions or would like to get in touch, please see my contact details below.
+
+Github profile: [gurdeep-ninja](https://github.com/gurdeep-ninja)
+
+Email address: [gurdeepbangar@protonmailsdsd.com](mailto:gurdeepbangar@protonmailsdsd.com)
