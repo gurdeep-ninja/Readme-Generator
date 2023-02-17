c1ccfec827dd12c27897901a7c990369fdf1d431
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-02-17 15:42:28 +0000

    Added a suite of questions

--- index.js
+++ index.js
@@ -7,9 +7,50 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const questions = [
     {
         type: 'input',
-        message: 'Enter project title',
+        message: 'Add project title:',
         name: 'title',
     },
+    {
+        type: 'input',
+        message: 'Add project description:',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Add installation guidelines:',
+        name: 'installation',
+    },  
+    {
+        type: 'input',
+        message: 'Add usage (instructions):',
+        name: 'usage',
+    },  
+    {
+        type: 'list',
+        message: 'Select a license type:',
+        choices: ["Apache License 2.0", "MIT License", "GNU General Public License v3.0","Mozilla Public License 2.0"],
+        name: 'license',
+    },  
+    {
+        type: 'input',
+        message: 'Please enter how others can contribute:',
+        name: 'contribution',
+    },  
+    {
+        type: 'input',
+        message: 'Add test instructions: ',
+        name: 'test',
+    },  
+    {
+        type: 'input',
+        message: 'Your github username: ',
+        name: 'github',
+    },  
+    {
+        type: 'input',
+        message: 'Your email address: ',
+        name: 'email',
+    },  
 ];
 
 // function to write README file
@@ -21,7 +62,7 @@ function init() {
     inquirer
     .prompt(questions)
     .then((response) =>
-      console.log(response.title)
+      console.log(response)
     );    
 
 }
